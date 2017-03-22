9eef6d7b99dc25bade30649eed12f0fb16e39bff
Rohan Khanna <rohan.kh@gmail.com>
2017-03-22 17:12:49 +0530

    animations included transitions in place

--- scratch1/script.js
+++ scratch1/script.js
@@ -2,6 +2,10 @@ var color = d3.scaleOrdinal()
     .domain(['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'D1', 'D2', 'E'])
     .range(["#0F4FA0", "#2196F3", "#4CAF50", "#8BC34A", "#FFB300", "#FFCA28", "#5B2A22", '#795548', '#F4201F']);
 
+var gradeToUnselectedListIndex = d3.scaleOrdinal()
+    .domain(['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'D1', 'D2', 'E'])
+    .range([0,1,2,3,4,5,6,7,8]);
+
 var barWidth = 50;
 var interBarPadding = 10;
 var margin = {
@@ -16,7 +20,6 @@ var margin = {
 var scalingConstant = 2;
 var unselectedList = [];
 var YScale = updateYscale(unselectedList);
-
 
 function updateYscale(ignoreList){
     return d3.scaleLinear()
@@ -47,10 +50,26 @@ var glevel = 0;
 
 update();
 
-function update(){
+function setYAttribute(d,j){
+    var unselectedSet = new Set(unselectedList);
+    if(j === 0)
+        glevel = 0;
+    if(unselectedSet.has(j))
+        return height - glevel;
+    glevel += YScale(d.value)
+    return height - glevel;
+}
 
-    canvas.selectAll('g') // selectAll then Data then Enter then Append // this is known as the d3 join pattern
-        .data(freqData)
+function setHeightAttribute(d,j){
+    var unselectedSet = new Set(unselectedList);
+    if(unselectedSet.has(j))
+        return 0;
+    return YScale(d.value);
+}
+
+function update() {    
+    canvas.selectAll('g')
+        .data(freqData)// selectAll then Data then Enter then Append // this is known as the d3 join pattern
         .enter()
         .append('g')
         .attr('transform',function (d,i) {
@@ -62,25 +81,8 @@ function update(){
         })
         .enter()
         .append('rect')
-        .attr('y',function(d,j){
-            var unselectedSet = new Set(unselectedList);
-
-            if(j === 0)
-                glevel = 0;
-
-            if(unselectedSet.has(j))
-                return 0;
-
-            glevel += YScale(d.value)
-            return height - glevel;
-        })
-        .attr('height',function(d,j){
-            // debugger;
-            var unselectedSet = new Set(unselectedList);
-            if(unselectedSet.has(j))
-                return 0;
-            return YScale(d.value);
-        })
+        .attr('y',setYAttribute)
+        .attr('height',setHeightAttribute)
         .attr('width',50)
         .attr('class',function(d){
             return 'class-' + d.grade;
@@ -93,8 +95,15 @@ function update(){
             unselectedSet.add(i);
             unselectedList = Array.from(unselectedSet);
             YScale = updateYscale(unselectedList);
-            d3.select('svg').selectAll('*').remove();
-            update();
+            // d3.select('svg').selectAll('*').remove();
+            // debugger;
+            canvas
+                .selectAll('g')
+                .selectAll('rect')
+                .transition()
+                .duration(1000)
+                .attr('y',setYAttribute)
+                .attr('height',setHeightAttribute);
         })
         .on('mouseover',function(d){
             d3.select(this)
@@ -104,9 +113,5 @@ function update(){
             d3.select(this)
                 .style('opacity', 1);
         });
-}
-
-function scale(value){
-    return value * scalingConstant;
 }
 
